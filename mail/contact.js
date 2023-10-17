40ec253fa7a4635e89ad803141e40796c6536711
ma-za-kpe <makpalyy@gmail.com>
2023-10-17 20:01:48 +0000

    add debbugging logic

--- mail/contact.js
+++ mail/contact.js
@@ -35,10 +35,11 @@ $(function () {
                     $('#contactForm').trigger("reset");
                 },
                 error: function (data) {
+                    console.log(JSON.stringify(data))
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding because " + data +". Please try again later!"));
+                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
                     $('#success > .alert-danger').append('</div>');
                     $('#contactForm').trigger("reset");
                 },
